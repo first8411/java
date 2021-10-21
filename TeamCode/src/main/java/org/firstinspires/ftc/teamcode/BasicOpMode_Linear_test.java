062ec64e035439f0e657973e8a682eaf58298025
drewTheNerd <63565940+drewTheNerd@users.noreply.github.com>
2021-10-21 16:58:12 -0500

    asdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear_test.java
@@ -50,9 +50,6 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-// hellooooo
-    // test
-
 @TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
 //@Disabled
 public class BasicOpMode_Linear_test extends LinearOpMode {
